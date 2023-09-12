9eeffec0d5de065138565b2f7106d965551af85e
Sabbir Ahmed Chowdhury <sabbirahmedchowdhury@gmail.com>
2023-09-12 14:57:09 +0600

    Finished the edit todos and other stuffs

--- api.py
+++ api.py
@@ -13,24 +13,28 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="template")
 
 
+# This is the main route to show the add todo page with the required parameters based on add, update or archive a todo.
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request, date: str = None, q: str = None):
+async def index(request: Request, date: str = None, q: str = None, todo_id: str = None):
     if q and not date:
-        return templates.TemplateResponse("index.html", {"request": request, "q": q})
+        return templates.TemplateResponse("index.html", {"request": request, "q": q, "todo_id": todo_id})
     if q and date:
-        return templates.TemplateResponse("index.html", {"request": request, "q": q, "date": str(date)})
+        return templates.TemplateResponse("index.html", {"request": request, "q": q, "date": str(date), "todo_id": todo_id})
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+# This route is to add the todos as well as update a todo.
 @app.post("/add_todo", response_class=HTMLResponse)
 async def add_todo(request: Request, todo: str = Form(...), todo_date: str = Form(...), todo_id: Optional[str] = Form(None)):
-    print(todo_id)
     try:
-       save_todos(todo,todo_date,todo_id)
-       return templates.TemplateResponse("confirm.html", {"request": request, "message": "Item inserted succussfully."})
+        save_todos(todo,todo_date,todo_id)
+        if todo_id:
+            return templates.TemplateResponse("confirm.html", {"request": request, "message": "Item updated succussfully.", "title": "Update Todo"})
+        return templates.TemplateResponse("confirm.html", {"request": request, "message": "Item added succussfully.", "title": "Add Todo"})
     except Exception as e:
-       return templates.TemplateResponse("confirm.html", {"request": request, "message": e})
+        return templates.TemplateResponse("confirm.html", {"request": request, "message": e})
     
+# This troute is to fetch the todos when the todo list page is loaded.
 @app.get("/get_todos", response_class=HTMLResponse)
 async def get_todos(request: Request):
     try:
@@ -39,6 +43,7 @@ async def get_todos(request: Request):
     except Exception as e:
         return templates.TemplateResponse("todo_list.html", {"request": request, "message": e})
 
+# This routes is to delete a todo which is basically change the status of a todo to list them as archived.
 @app.delete("/delete_todo/{date}/{id}", response_class=HTMLResponse)   
 async def update_todo(request: Request, date: str, id: str):
     try:
@@ -47,6 +52,7 @@ async def update_todo(request: Request, date: str, id: str):
     except Exception as e:
         return templates.TemplateResponse("includes/card.html", {"request": request, "message": e})
 
+# This route is to get the list of archived todos.
 @app.get("/get_archive_todos", response_class=HTMLResponse)
 async def get_archived_todos(request: Request):
     try:
@@ -55,6 +61,7 @@ async def get_archived_todos(request: Request):
     except Exception as e:
         return templates.TemplateResponse("todo_list.html", {"request": request, "message": e}) 
 
+# This route is to get the todos by date while selecting from the calendar in todo list.
 @app.get("/get_todos_by_day", response_class=HTMLResponse) 
 async def get_todos_by_date(request: Request, date: str = None):
     try:
@@ -62,8 +69,3 @@ async def get_todos_by_date(request: Request, date: str = None):
         return templates.TemplateResponse("includes/card.html", {"request": request, "todos": todos_by_date, "title": f"Todo List of {date}", "status": 1})
     except Exception as e:
         return templates.TemplateResponse("includes/card.html", {"request": request, "message": e})     
-
-
-
-
-       
--- db.py
+++ db.py
@@ -2,6 +2,9 @@ from pymongo import MongoClient
 from bson import ObjectId
 from datetime import date
 
+HOST = "localhost"
+PORT = 27017
+
 # Let us create a context manager for the database.
 class DBConnect:
     def __init__(self,host, port):
@@ -10,32 +13,35 @@ class DBConnect:
         self.conn = None
 
     def __enter__(self):
-        print("Enter")
+        #print("Enter")
         self.conn = MongoClient(self.host, self.port)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("Exit")
+        #print("Exit")
         self.conn.close()
 
 
 # This function is used to insert a single todo and update an existing todo 
-# while adding back from the archive
+# while adding back from the archive. Also, update a todo or todo date as per needed.
 def save_todos(todo: str, todo_date: str, todo_id: str = None):
-    with DBConnect("localhost", 27017) as c:
+    with DBConnect(HOST, PORT) as c:
         p = c.conn.todos.todo
         checkIfExists = p.count_documents({"todo": todo})
-        print(todo_id)
+        
         if checkIfExists == 0:
+            if todo_id != None:
+                delete_todo(todo_id, todo_date)
             p.insert_one({"todo": todo, "date": todo_date, "status": 1})
         else:
-            updateOpt = {"_id": todo_id}
+            print(todo_id)
+            updateOpt = {"_id": ObjectId(todo_id)}
             changeTodo = {"$set": {"todo": todo, "date": todo_date, "status": 1}}  
             p.update_one(updateOpt, changeTodo)  
         
 # This function is used to get the active todo list of the current date
 def get_todo_list():
-    with DBConnect("localhost", 27017) as c:
+    with DBConnect(HOST, PORT) as c:
         p = c.conn.todos.todo
         date1 = str(date.today())
         list_todo = list(p.find({"date": date1, "status": 1}).sort([('$natural',-1)])) #get the latest/last item first
@@ -44,7 +50,7 @@ def get_todo_list():
 # This function is used to update the status of a todo rather than deleting it. After updating, the list of 
 # todos by date is returned ro regenerate the list.
 def delete_todo(id: str, date: str):
-    with DBConnect("localhost", 27017) as c:
+    with DBConnect(HOST, PORT) as c:
         p = c.conn.todos.todo
         delOpt = {"_id": ObjectId(id)}
         changeStatus = {"$set": { "status": 0 }}
@@ -53,13 +59,14 @@ def delete_todo(id: str, date: str):
 
 # This function is used to get the archived todo list.
 def get_archive_todos():
-    with DBConnect("localhost", 27017) as c:
+    with DBConnect(HOST, PORT) as c:
         p = c.conn.todos.todo
         arch_todo = list(p.find({"status": 0}).sort([('$natural',-1)]))
         return arch_todo     
 
+# This function is used to get the datewise todo while selecting from the calender in todo list.
 def get_datewise_todos(q):
-    with DBConnect("localhost", 27017) as c:
+    with DBConnect(HOST, PORT) as c:
         p = c.conn.todos.todo
         list_todo = list(p.find({"date": q, "status": 1}).sort([('$natural',-1)])) #get the latest item first
         return list_todo       
